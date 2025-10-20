30f7411c84f1a09d103e866c8042372014ad23da
kranthi.j <kranthi.j@xenarcs.com>
2025-10-20 22:02:00 +0530

    skeleton fix

--- src/components/SkeletonCard.tsx
+++ src/components/SkeletonCard.tsx
@@ -3,7 +3,7 @@ export default function SkeletonCard() {
     <div className="bg-[#1a1f2e] rounded overflow-hidden animate-pulse h-full flex flex-col">
       <div className="aspect-square bg-gray-800" />
 
-      <div className="p-4 flex-1">
+      <div className="p-4 flex-1 flex-row">
         <div className="space-y-2 mb-3">
           <div className="h-4 bg-gray-700 rounded w-3/4" />
           <div className="h-3 bg-gray-700 rounded w-1/2" />
@@ -12,5 +12,5 @@ export default function SkeletonCard() {
         <div className="h-5 bg-gray-700 rounded w-1/4 ml-auto" />
       </div>
     </div>
-  )
+  );
 }
